d8d401add8ab46b766bb651f9e05aa49c3fa04df
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-25 15:27:26 +0000

    created flask templetes

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+// sideNav initalization 
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
