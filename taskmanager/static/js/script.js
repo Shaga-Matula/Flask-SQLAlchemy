95215f3edd34805dde51765a2d982ad99da52bca
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-25 17:15:33 +0000

    create form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-// sideNav initalization 
-  let sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
 });
