8230b1c46215182ff0cbe3c9b827fdfb6e3445a1
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-25 11:09:18 +0000

    env.py

---
+++ back.env.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault("IP","0.0.0.0")
+os.environ.setdefault("PORT","5000")
+os.environ.setdefault("Secret_key","ANY_KEY")
+os.environ.setdefault("DEBUG","True")
+os.environ.setdefault("DEVOLOPMENT","True")
+os.environ.setdefault("DB_URL","postgresql:///taskmanager")
